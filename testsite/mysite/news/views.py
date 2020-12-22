d57e63e39a11b77102cc1a6b4fe93834d260ad9f
kamolisrailov <mr.lengdon99@gmail.com>
2020-12-23 01:46:43 +0500

    photo news added

--- testsite/mysite/news/views.py
+++ testsite/mysite/news/views.py
@@ -1,12 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .models import News
+from .models import News, Category
 
 
 def index(request):
     news = News.objects.all()
+    categories = Category.objects.all()
     context = {
         'news': news,
-                'title': 'Список новостей'
+        'title': 'Список новостей',
+        'categories': categories,
     }
     return render(request, 'news/index.html', context)
