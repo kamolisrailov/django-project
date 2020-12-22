c3ed2cb45e8a0b8876af9776e6c2ec178d0dfee9
kamolisrailov <mr.lengdon99@gmail.com>
2020-12-23 02:05:21 +0500

    sidebar and category pages added

--- testsite/mysite/news/urls.py
+++ testsite/mysite/news/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import index
+from .views import index, get_category
 
 urlpatterns = [
-    path('', index)
+    path('', index),
+    path('category/<int:category_id>/', get_category),
 
 ]
